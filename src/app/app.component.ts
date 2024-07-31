091cabe9a70c47831b9447b06aaf92f0535837ec
Carles Labrana <carles.labrana@idealiveconsulting.com>
2024-07-31 13:43:07 +0200

    Notation Changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,15 +25,17 @@ export class AppComponent {
   constructor(private questionFactoryService: QuestionFactoryService) {}
 
  createQuestion() {
+    //Crea les dades
     const question = this.questionFactoryService.createQuestion(this.selectedQuestionType);
     this.questions.push(question);
-    const activityComponent = this.getQuestionComponent(this.selectedQuestionType);
-    const componentRef = this.container.createComponent(activityComponent);
-    componentRef.instance.question = question;
+    //Crea el Component
+    const questionComponent = this.getQuestionComponent(this.selectedQuestionType);
+    const componentRef = this.container.createComponent(questionComponent);
+    componentRef.instance.question = question;  //Input dades
   }
 
 
-  private getQuestionComponent(type: string) {
+  private getQuestionComponent(type: string) : Question {
     const componentMap: { [key: string]: any; } = {
       'text': TextComponent,
       'longtext': TextComponent
